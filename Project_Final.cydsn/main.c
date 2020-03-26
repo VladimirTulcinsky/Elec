cd1baee7d0ec36c0d2a1b941ddd697bafedad490
mathiashanquiniaux <mathias.hanquiniaux@gmail.com>
2020-03-26 12:21:09 +0100

    appui boutton1 allume 4 led clean

--- Project_Final.cydsn/main.c
+++ Project_Final.cydsn/main.c
@@ -11,19 +11,31 @@
 */
 #include "project.h"
 
-int main(void)
-{
-    CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
-    for(;;)
+  // Function to turn on all LEDs when you press on SW1
+ void appuiSW1()
     {
       Led_1_Write( SW1_Read() );
       Led_2_Write( SW1_Read() );
       Led_3_Write( SW1_Read() );
       Led_4_Write( SW1_Read() );
     }
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+  
+   
+    
+    for(;;)
+    {
+        appuiSW1();
+      
+    }
 }
+
+
 
 /* [] END OF FILE */
